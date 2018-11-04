30d5869ea0a132fbfb2f99c1ec06d456957bca9c
TT <tom_takasawa@icloud.com>
2018-11-04 14:15:19 -0500

    initializing project

---
+++ iOS-GoodCodeExerciseSample/iOS-GoodCodeExerciseSampleTests/iOS_GoodCodeExerciseSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_GoodCodeExerciseSampleTests.swift
+//  iOS-GoodCodeExerciseSampleTests
+//
+//  Created by Tomoki Takasawa on 11/4/18.
+//  Copyright © 2018 Tomoki Takasawa. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_GoodCodeExerciseSample
+
+class iOS_GoodCodeExerciseSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
