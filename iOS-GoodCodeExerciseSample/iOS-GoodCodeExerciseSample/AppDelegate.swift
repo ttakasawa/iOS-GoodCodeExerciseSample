066293a87de2c3f0fe01e353503a57e40b3294cc
TT <tom_takasawa@icloud.com>
2018-11-06 01:56:53 -0500

    sample images change

--- iOS-GoodCodeExerciseSample/iOS-GoodCodeExerciseSample/AppDelegate.swift
+++ iOS-GoodCodeExerciseSample/iOS-GoodCodeExerciseSample/AppDelegate.swift
@@ -23,7 +23,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        UINavigationBar.appearance().barTintColor = UIColor.black
+        UINavigationBar.appearance().barTintColor = UIColor.darkGray
+        UINavigationBar.appearance().tintColor = .white
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         
